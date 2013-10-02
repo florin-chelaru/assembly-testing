0bc666920e50b4d327bddeae786d39d6975ab0b5
Christopher Michael Hill <cmhill@umd.edu>
2013-10-02 12:34:49 -0400

    Added info how to build histogram

--- data/mate_pair/example/run_bowtie2.sh
+++ data/mate_pair/example/run_bowtie2.sh
@@ -5,3 +5,6 @@ bowtie2-build ../rhodobacter.assembly.fasta rhodobacter.bt2
 
 # Run bowtie2 to get the mate pair alignments.  400 and 600 are the upper and lower bounds to search for a proper mate pair alignment.
 bowtie2 -x rhodobacter.bt2 -f -1 ../rhodobacter.sequences.1.fasta -2 ../rhodobacter.sequences.2.fasta -I 400 -X 600 -S rhodobacter.sequences.sam
+
+# Quick command to build a histogram of the insert sizes.
+awk '{if (int($9) > 0) print $9}' rhodobacter.sequences.sam | sort -nr | uniq -c > rhodobacter.sequences.insert_sizes
