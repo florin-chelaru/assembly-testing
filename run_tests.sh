35e7ef600f660b04be14af6a29f7e671863bd29e
Christopher Michael Hill <cmhill@umd.edu>
2013-10-28 23:48:38 -0400

    Added skeleton test case

---
+++ run_tests.sh
@@ -0,0 +1,16 @@
+#! /usr/bin/sh
+
+./testcases/tc_trivial/run_test.sh
+return_code=$?
+
+if [ ${return_code} -eq 0 ]; then
+    echo "PASSED"
+else
+    echo "FAILED"
+fi
+
+if [ ${return_code} -ne 0 ]; then
+    echo "PASSED"
+else
+    echo "FAILED"
+fi
