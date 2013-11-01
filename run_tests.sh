ae2e17fc3d89bbe1c94ca9f34a58365aa4983110
Christopher Michael Hill <cmhill@umd.edu>
2013-11-01 16:14:15 -0400

    Added skeleton test case.

--- run_tests.sh
+++ run_tests.sh
@@ -1,16 +1,35 @@
 #! /usr/bin/sh
 
-testcases/tc_trivial/run_test.sh
-return_code=$?
+timestamp=`date +%FT%R:%S`
 
-if [ ${return_code} -eq 0 ]; then
-    echo "PASSED"
-else
-    echo "FAILED"
-fi
+echo "<testsuites>"
+echo "    <testsuite name=\"Assembler testing\" tests=\"2\" failures=\"1\" timestamp=\"${timestamp}\">"
 
-if [ ${return_code} -ne 0 ]; then
-    echo "PASSED"
-else
-    echo "FAILED"
-fi
+for tc in testcases/* ; do
+    tc=`basename ${tc}`
+    testcases/${tc}/run_test.sh
+    return_code=$?
+
+    if [ ${return_code} -eq 0 ]; then
+        echo "<testcase name=\"${tc}\" classname=\"None\"/>"
+        echo "</testcase>"
+    else
+        echo "<testcase name=\"${tc} fail\" classname=\"None\">"
+        echo "     <error message=\"Failed\">Failed2.</error>"
+        echo "</testcase>"
+    fi
+
+    if [ ${return_code} -ne 0 ]; then
+        echo "<testcase name=\"${tc}\" classname=\"None\"/>"
+        echo "</testcase>"
+    else
+        echo "<testcase name=\"${tc} fail\" classname=\"None\">"
+        echo "     <error message=\"Failed\">Failed2.</error>"
+        echo "</testcase>"
+    fi
+
+done
+
+echo "    </testsuite>"
+echo "</testsuites>"
+
