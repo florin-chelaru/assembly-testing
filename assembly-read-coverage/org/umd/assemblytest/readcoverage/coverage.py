5a39fc20d8b80897f71f0c9ca87e9778de8eb9b9
Konstantinos Zampogiannis <kzampog@gmail.com>
2013-11-09 03:46:32 -0500

    coverage refactoring, implemented statistics (gaussian, binary vectors output)

--- assembly-read-coverage/org/umd/assemblytest/readcoverage/coverage.py
+++ assembly-read-coverage/org/umd/assemblytest/readcoverage/coverage.py
@@ -1,46 +1,53 @@
 '''
 Created on Nov 7, 2013
 
-@author: jason & kostas        
+@author: jason & kostas
 '''
 import numpy as np
 from samfile import SamFile
 import matplotlib.pyplot as plt
 
-class ContigData(object):
+'''
+Read a FASTA assembly file and store the {contig_id, contig_length}
+pairs in a dictionary.
+'''
+
+def parse_fasta_file(filename):
+    contig_length = dict()
+    try :
+        with open(filename) as f:
+            current_key = None
+            for line in f:
+                line = line.strip();
+                if line[0] == '>':
+                    current_key = line[1:]  # update current key
+                    contig_length[current_key] = 0
+                else:
+                    contig_length[current_key] += (len(line))
+    except EnvironmentError as err:
+        print "Unable to open FASTA file: {}".format(err);
+    return contig_length
+
+def write_coverage_plot(cov, contig_id, filename):
+    plt.plot(cov.contig_coverage[contig_id]);
+    plt.xlabel('Window index');
+    plt.ylabel('Coverage');
+    plt.savefig(filename);
+    # plt.show();
+
+class ContigBPCoverage:
+
+    def __init__(self, samfile, contig_length):
+        self.contig_length = contig_length
+        self.contig_coverage = dict()
+        for contig_id in contig_length:
+            cov = np.array([len(samfile.coverage(contig_id, pos, 1)) for pos in range(contig_length[contig_id])], dtype=np.float32)
+            self.contig_coverage[contig_id] = cov
+
+class ContigWindowCoverage(object):
 
     '''
-    Read a Fasta assembly file and store the {contig_id, contig_length}
-    pairs in a dictionary.
-    '''
-
-    def __init__(self, filename):
-        '''
-        Constructor takes a FASTA filename and stores
-        the data in the dictionary.
-        '''
-        self.contig_length = dict();
-        self._fastafile = filename;
-
-        # parse the FASTA file
-        try :
-            with open(filename) as f:
-                current_key = None
-                for line in f:
-                    line = line.strip();
-                    if line[0] == '>':
-                        current_key = line[1:]  # update current key
-                        self.contig_length[current_key] = 0
-                    else:
-                        self.contig_length[current_key] += (len(line))  # don't count '\n'
-        except EnvironmentError as err:
-                print "Unable to open FASTA file: {}".format(err); 
-
-
-class ContigCoverage(object):
-
-    '''
-    Computes the coverage of a contig given a 
+    Computes the coverage of a contig given a
     specified window size and sliding step for the window.
     Note that binning is a special case of sliding windows when the step size
     just happens to be equal to window length.
@@ -48,65 +55,51 @@ class ContigCoverage(object):
 
     '''
     Constructor
-    @param samfile a Samfile object 
+    @param samfile a Samfile object
     @param contig_data a ContigData object
     @param window_length a positive integer representing the window length
     @param step_size a positive integer representing the step size for the window.
     '''
-    def __init__(self, samfile, contig_data, window_length, step_size):
-
-        # Sanity checking -- NEEDS FIX!
-        
+    def __init__(self, bp_coverage, window_length, step_size):
+        # Sanity checking
         if window_length < 0 or step_size < 0:
             raise RuntimeError, "ContigCoverage constructor: Provided negative argument."
-        if samfile is None or contig_data is None:
+        if bp_coverage is None:
             raise RuntimeError, "ContigCoverage constructor: Provided None argument."
-        
         # Main algorithm
         self.window_length = window_length
         self.step_size = step_size
-        self.contig_length = contig_data.contig_length
-        self.contig_coverage = {};  # associates contigs with their windows' coverages.
-        self.contig_window_starting_points = {}
-        self.__calcBPCoverage__(samfile, contig_data)
-        cl = contig_data.contig_length
+        self.contig_length = bp_coverage.contig_length
+        self.contig_coverage = {};  # associates contigs with their windows' coverages
+        self.contig_window_start_index = {}
         window = np.ones(window_length) / window_length
-        for contig_id in cl:
+        for contig_id in bp_coverage.contig_length:
             if window_length == 1:
-                cov = self._bplevelcoverage_[contig_id]
+                cov = bp_coverage.contig_coverage[contig_id]
             else:
-                cov=np.convolve(self._bplevelcoverage_[contig_id], window, 'valid')
+                cov = np.convolve(bp_coverage.contig_coverage[contig_id], window, 'valid')
             starting_points = range(len(cov))
-            if step_size > 1: # convolve for speed
+            if step_size > 1:
                 cov = cov[0::step_size]
                 starting_points = starting_points[0::step_size]
             self.contig_coverage[contig_id] = cov
-            self.contig_window_starting_points[contig_id] = starting_points
-                
-
-    def __calcBPCoverage__(self, samfile, contig_data):
-        self._bplevelcoverage_ = dict()
-        cl = contig_data.contig_length
-        for contig_id in cl:
-            cov = np.array([len(samfile.coverage(contig_id, pos, 1)) for pos in range(cl[contig_id])], dtype=np.float32)
-            self._bplevelcoverage_[contig_id] = cov
-        
-    
-    def write_coverage_plot(self, contig_id, filename):
-        plt.plot(coverage.contig_coverage[contig_id]);
-        plt.xlabel('Window index');
-        plt.ylabel('Coverage');
-        plt.savefig(filename);
-        plt.show();
+            self.contig_window_start_index[contig_id] = starting_points
 
 if __name__ == '__main__':
+    samfile = SamFile.read('../../../../../tutorial/read_coverage/influenza-A.sam')
+    cdata = parse_fasta_file('../../../../../data/influenza-A/influenza-A.assembly.fasta')
+    bp_cov = ContigBPCoverage(samfile, cdata)
+    w_cov = ContigWindowCoverage(bp_cov, 100, 1)
+    # write_coverage_plot(w_cov, '8', 'dok.png')
+    print len(w_cov.contig_coverage)
+    cov = w_cov
+    for c in cov.contig_coverage:
+        print "Contig {0} has {1} bp's, {2} windows and coverage: {3}".format(c, cov.contig_length[c], len(cov.contig_coverage[c]), cov.contig_coverage[c]);
+    '''
     cdata = ContigData('../../../../../data/influenza-A/influenza-A.assembly.fasta');
     samfile = SamFile.read('../../../../../tutorial/read_coverage/influenza-A.sam');
-    coverage = ContigCoverage(samfile, cdata, 100, 1);
-    print "asdfasdfasdf"
+    coverage = ContigCoverage(samfile, cdata, 200, 100);
     for contig in coverage.contig_coverage:
         print "Contig with id {0} has {1} base-pairs and {2} windows.".format(contig, cdata.contig_length[contig], len(coverage.contig_coverage[contig]));
         print "Contig with id {0} has a window coverage of {1}.".format(contig, str(coverage.contig_coverage[contig]));
-
-    print 'asdfasdf {0} asdf'.format(len(samfile.coverage('1', 3, 3)));
-    #coverage.write_coverage_plot('1', 'dok.png');
+    '''
--- assembly-read-coverage/org/umd/assemblytest/readcoverage/statistics.py
+++ assembly-read-coverage/org/umd/assemblytest/readcoverage/statistics.py
@@ -4,34 +4,73 @@ Created on Nov 7, 2013
 @author: jason & kostas
 '''
 import numpy as np
-#from samfile import SamFile
-from coverage import ContigCoverage
+from coverage import parse_fasta_file, ContigBPCoverage, ContigWindowCoverage
+from samfile import SamFile
 # import matplotlib.pyplot as plt
 
-class CoverageStats(object):
-    
-    def gaussiantest(self, contig_coverage, threshold):
-    
-        if threshold < 0:
-            raise RuntimeError, "gaussiantest(): Threshold should be non-negative."
-        idstocov = contig_coverage.contig_coverage
-        idstolength = contig_coverage.contig_length
-        win_length = contig_coverage.window_length
-        idstowindowstartpts = contig_coverage.contig_window_starting_points
-        allcovs = np.ndarray.flatten([idstocov[ids] for ids in idstocov])
-        mu = np.mean(allcovs)
-        stdev = np.std(allcovs)
-        t_high = mu + threshold * stdev
-        t_low = mu - threshold * stdev
-        bad_windows = {} # maps ids to numpy arrays
-        for c_id in idstocov:
-            indices_high = idstocov[c_id] > t_high
-            indices_low = idstocov[c_id] < t_low
-            a = np.array(range(indices_high))
-            b = np.array(range(indices_low))
-            #bad_windows_above needs to be a list of triples
-            bad_windows_above = [(idstowindowstartpts[c_id][index], 
-                                  idstowindowstartpts[c_id][index] + win_length - 1,
-                                  idstocov[c_id][index]) 
-                                 for index in a[indices_high]]
-            bad_windows[c_id] = 
+class CoverageStatistics(object):
+
+    def __get_all_values__(self, cov):
+        # Beautiful code
+        allvals = [cov.contig_coverage[cid].tolist() for cid in cov.contig_coverage]
+        allvals = np.array([item for sublist in allvals for item in sublist])
+        return allvals
+
+    def __map_win_to_bp__(self, bool_mask, cov, cid):
+        bp = np.zeros(cov.contig_length[cid], dtype=np.bool)
+        mask_ind = np.flatnonzero(bool_mask)
+        for i in range(len(mask_ind)):
+            bp_pos = cov.contig_window_start_index[cid][mask_ind[i]]
+            bp[bp_pos:(bp_pos + cov.window_length)] = True
+        return bp
+
+    def __init__(self, cov, test_type, param):
+        self.test_type = test_type
+        if test_type == 'Gaussian':
+            if param < 0:
+                raise RuntimeError, "CoverageStatistics(): param cannot be negative for this test type."
+            allvals = self.__get_all_values__(cov)
+            self.mu = np.mean(allvals)
+            self.sigma = np.std(allvals)
+            self.t_high = self.mu + param * self.sigma
+            self.t_low = self.mu - param * self.sigma
+        elif test_type == 'Percentile':
+            if param < 0:
+                raise RuntimeError, "CoverageStatistics(): param cannot be negative for this test type."
+            allvals = self.__get_all_values__(cov)
+            self.t_high = 1
+            self.t_low = 0
+        else:
+            raise RuntimeError, "CoverageStatistics(): unknown test type."
+        # Test coverage values against t_high and t_low
+        self.contig_coverage = cov.contig_coverage
+        self.contig_length = cov.contig_length
+        self.contig_overcovered_windows = {}
+        self.contig_undercovered_windows = {}
+        self.contig_overcovered_bps = {}
+        self.contig_undercovered_bps = {}
+        for cid in cov.contig_coverage:
+            win_over = cov.contig_coverage[cid] > self.t_high
+            win_under = cov.contig_coverage[cid] < self.t_low
+            bp_over = self.__map_win_to_bp__(win_over, cov, cid)
+            bp_under = self.__map_win_to_bp__(win_under, cov, cid)
+            # binary vectors
+            self.contig_overcovered_windows[cid] = win_over
+            self.contig_undercovered_windows[cid] = win_under
+            self.contig_overcovered_bps[cid] = bp_over
+            self.contig_undercovered_bps[cid] = bp_under
+
+if __name__ == '__main__':
+    samfile = SamFile.read('../../../../../tutorial/read_coverage/influenza-A.sam')
+    cdata = parse_fasta_file('../../../../../data/influenza-A/influenza-A.assembly.fasta')
+    bp_cov = ContigBPCoverage(samfile, cdata)
+    w_cov = ContigWindowCoverage(bp_cov, 200, 1)
+    st = CoverageStatistics(w_cov, 'Gaussian', 2.3)
+    c = '8'
+    # plt.plot(w_cov.contig_coverage[c])
+    # plt.savefig('cov.png')
+    # plt.plot(st.contig_overcovered_windows[c])
+    # plt.savefig('win_over.png')
+    # plt.plot(st.contig_undercovered_bps[c])
+    # plt.savefig('bp_under.png')
+
