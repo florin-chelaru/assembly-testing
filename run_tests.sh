38e53f7957ed57d7add1a75c4219aa58c7c70da0
Jason Filippou <jason.filippou@gmail.com>
2013-12-09 18:49:19 -0500

    finish run_tests.sh

--- run_tests.sh
+++ run_tests.sh
@@ -4,25 +4,28 @@ timestamp=`date +%FT%R:%S`
 tests=0
 failures=0
 
-for tc in testcases/* ; do
-    tc=`basename ${tc}`
-    cd ./testcases/${tc}/
-    sh run_test.sh
-    return_code=$?
-    cd ../../
+for tc in testcases/group_1/* ; do
+    if [ -d $tc ] 
+    then
+	    tc=`basename ${tc}`
+	    cd ./testcases/group_1/${tc}/
+	    echo $tc
+	    sh run_test.sh
+    	    return_code=$?
+	    cd ../../../
 
-    tests=$((tests + 1))
+	    tests=$((tests + 1))
 
-    if [ ${return_code} -eq 0 ]; then
-        echo "<testcase name=\"${tc}\" classname=\"None\">" >> tmp.results
-        echo "</testcase>" >> tmp.results
-    else
-        echo "<testcase name=\"${tc} fail\" classname=\"None\">" >> tmp.results
-        echo "<error message=\"Failed\">Failed.</error>" >> tmp.results
-        echo "</testcase>" >> tmp.results
-
-        failures=$((failures + 1))
-    fi
+	    if [ ${return_code} -eq 0 ]; then
+        	echo "<testcase name=\"${tc}\" classname=\"None\">" >> tmp.results
+	        echo "</testcase>" >> tmp.results
+	    else
+        	echo "<testcase name=\"${tc} fail\" classname=\"None\">" >> tmp.results
+	        echo "<error message=\"Failed\">Failed.</error>" >> tmp.results
+	        echo "</testcase>" >> tmp.results
+        	failures=$((failures + 1))
+	    fi
+      fi
 
 done
 
---
+++ testcases/group_1/tc_1/README.txt
@@ -0,0 +1,7 @@
+count_10000_length_500_error_0:
+tc_1	practical_test_influenza-A.assembly.fasta_1_100000_200000_1_300000_400000_1_500000_600000_1_700000_750000_1_800000_820000
+tc_2	practical_test_influenza-A.assembly.fasta_1_100000_600000
+tc_3	practical_test_influenza-A.assembly.fasta_1_500_1500
+tc_4	practical_test_influenza-A.assembly.fasta_1_500_1500_2_500_1500
+tc_5	practical_test_influenza-A.assembly.fasta_2_200_250_3_100_400
+
