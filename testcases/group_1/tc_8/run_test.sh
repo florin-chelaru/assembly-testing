4a6b8353af7b7d3dc8135a2c9838dac64bea64c9
Jason Filippou <jason.filippou@gmail.com>
2013-12-09 18:49:19 -0500

    finish run_tests.sh

---
+++ testcases/group_1/tc_8/run_test.sh
@@ -0,0 +1,9 @@
+#! /usr/bin/sh
+
+rm -rf ./out/*;
+python ../../../assembly-read-coverage/org/umd/assemblytest/readcoverage/clipper.py -a assembly.fasta -r reads.fasta -w 200 -s 1 -t Gaussian -p 1.6 
+rm -rf ./out/*.sam ./out/*.bt2 ./out/*.stdout ./out/*.stderr
+python ../../../assembly-read-coverage-test/test/org/umd/assemblytest/readcoverage/pythia.py -m ./out/*_OVER_BP.cov -o oracle.oracle > pythia.stdout
+cat pythia.stdout 
+pythiaFlag=$?
+exit $pythiaFlag
